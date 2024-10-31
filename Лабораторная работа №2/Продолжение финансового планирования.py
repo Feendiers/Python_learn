895ffc7f9be0749fb17dd5921925b7c6d6e96f3f
Feendiers <155818480+Feendiers@users.noreply.github.com>
2024-10-31 22:06:17 +0300

    newUpdate Продолжение финансового планирования.py
    Округление финального результата выполнено при помощи метода ceil модуля math

--- Лабораторная работа №2/Продолжение финансового планирования.py
+++ Лабораторная работа №2/Продолжение финансового планирования.py
@@ -1,3 +1,5 @@
+import math
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -7,10 +9,7 @@ i = months
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 while i > 0:
     money_capital += spend - salary
-    spend *= 1.03
+    spend *= 1 + increase
     i -= 1
-for j in range(20000):
-    if  0 < j - money_capital < 1:
-        print(j)
-        money_capital = j
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", math.ceil(money_capital))
+
