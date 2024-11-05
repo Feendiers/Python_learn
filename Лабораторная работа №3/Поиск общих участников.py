b83d0aabd3b09d5b400de819046fd2ef4ffea7fb
Feendiers <155818480+Feendiers@users.noreply.github.com>
2024-11-05 18:37:51 +0300

    Update Поиск общих участников.py
    Добавлены комментарии, а также код изменен под лучшее соответствие выданному материалу

--- Лабораторная работа №3/Поиск общих участников.py
+++ Лабораторная работа №3/Поиск общих участников.py
@@ -6,14 +6,17 @@ participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
 def find_common_participants(str1, str2, sept = ','):
-    strarr1 = str1.split(sept)
-    strarr2 = str2.split(sept)
-    arrall = []
-    for i in strarr1:
-        for j in strarr2:
-            print(i, j)
-            if i == j:
-                arrall.append(i)
-    arrall.sort()
-    return arrall
+    # Преобразую строковые объекты в множества
+    arr1 = set(str1.split(sept))
+    arr2 = set(str2.split(sept))
+    total = arr1 & arr2#Определение общих элементов
+    #Проверяю есть ли общие элементы и если есть, то
+    #преобразую множество в список, затем сортирую его
+    #по возрастанию и возвращаю отсортированный список с
+    #общими элементами у двух изначальных строк
+    if total:
+        final = list(total)
+        final.sort()
+        return final
+#Проверяю работу функции, функция в качестве аргументов принимает две строки и разделитель для них
 print(find_common_participants(participants_first_group, participants_second_group, "|"))
