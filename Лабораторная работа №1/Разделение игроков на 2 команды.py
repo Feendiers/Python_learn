b38eeb149428de0cc6016e960c87081189bfda4f
Feendiers <155818480+Feendiers@users.noreply.github.com>
2024-11-19 22:41:50 +0300

    Update Разделение игроков на 2 команды.py
    Добавлены комментарии

--- Лабораторная работа №1/Разделение игроков на 2 команды.py
+++ Лабораторная работа №1/Разделение игроков на 2 команды.py
@@ -1,10 +1,10 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
-# индекс середины
+# Определяю середину изначального массива
 middle_index = len(list_players) // 2
-
-first_team = list_players[:middle_index]
+# При помощи среза массива создаю 2 новых массива
+first_team = list_players[:middle_index]  
 second_team = list_players[middle_index:]
-
+# Вывожу новые получившиеся массивы
 print(first_team)
 print(second_team)
